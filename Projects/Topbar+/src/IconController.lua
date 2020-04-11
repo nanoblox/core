1fb4174c11d10fc67808437af093dfad0ffd8680
ForeverHD <ben.horton.business@gmail.com>
2020-04-11 13:04:24 +0100

    Automation test 3

--- Projects/Topbar+/src/IconController.lua
+++ Projects/Topbar+/src/IconController.lua
@@ -118,5 +118,5 @@ function TopbarController:removeIcon(name)
 end
 
 
--- hello ben i like pie (2) but pineapple is cool (4)
+-- hello ben i like pie (2) but pineapple is cool (5)
 return TopbarController
