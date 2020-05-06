d54bedf5e152bbeeba61c861aa5a0bfb5fc4cd0c
ForeverHD <ben.horton.business@gmail.com>
2020-05-07 00:35:29 +0100

    Reduced charOffset to -1.4 to prevent precise detection bug when hitting internal parts

--- Projects/Zone+/src/Zone.lua
+++ Projects/Zone+/src/Zone.lua
@@ -235,7 +235,7 @@ function Zone:getPlayer(player)
 	local char = player.Character
 	local hrp = char and char:FindFirstChild("HumanoidRootPart")
 	if hrp then
-		local charOffset = hrp.Size.Y * -1.5
+		local charOffset = hrp.Size.Y * -1.4
 		local origin = hrp.Position + Vector3.new(0, charOffset, 0)
 		local hitValidPart = self:castRay(origin, self.groupParts)
 		return hitValidPart
