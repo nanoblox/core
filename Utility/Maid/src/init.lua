aef9b1d1642dab2cce3f63d7893d3a5457ab4a7c
ForeverHD <ben.horton.business@gmail.com>
2020-05-26 11:57:26 +0100

    Added Promise dependency

--- Utility/Maid/src/init.lua
+++ Utility/Maid/src/init.lua
@@ -1,4 +1,5 @@
 local DirectoryService = require(4926442976)
+local Promise = require(5091723186)
 local maid = script.Maid
 DirectoryService:createDirectory("ReplicatedStorage.HDAdmin", {maid})
 return require(maid)
