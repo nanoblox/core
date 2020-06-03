177ddbef4adbe42c7e404a9a6d34519c9a457e7b
ForeverHD <ben.horton.business@gmail.com>
2020-06-03 13:22:28 +0100

    Introduced three core folders: temp, perm and backup

--- Projects/DataStore+/src/User.lua
+++ Projects/DataStore+/src/User.lua
@@ -5,29 +5,35 @@ local players = game:GetService("Players")
 local runService = game:GetService("RunService")
 local teleportService = game:GetService("TeleportService")
 local replicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 local HDAdmin = replicatedStorage:WaitForChild("HDAdmin")
 local Signal = require(HDAdmin:WaitForChild("Signal"))
 local Maid = require(HDAdmin:WaitForChild("Maid"))
 local TableModifiers = require(script.Parent.TableModifiers)
 local User = {}
 User.__index = User
-setmetatable(User, TableModifiers)
 
 
 
 -- CONSTRUCTOR
 function User.new(dataStoreName, key)
-	local self = TableModifiers.new()
+	local self = {}
 	setmetatable(self, User)
 	
-	-- Config
-	local currentTime = os.time()
+	-- Maid
 	local maid = Maid.new()
 	self._maid = maid
-	self.sessionId = httpService:GenerateGUID(false)
-	self.isNewData = nil
-	self.dataLoaded = maid:give(Signal.new())
-	self.failDataPresent = maid:give(Signal.new())
+	
+	-- Main
+	self.temp = {}
+	self.perm = {}
+	self.backup = {}
+	maid:give(TableModifiers.apply(self.temp))
+	maid:give(TableModifiers.apply(self.perm))
+	maid:give(TableModifiers.apply(self.backup))
+	
+	-- Config
+	local currentTick = tick()
 	self.onlySaveDataWhenChanged = true
 	self.teleportPlayerAwayOnFail = false
 	self.autoSave = true
@@ -38,32 +44,33 @@ function User.new(dataStoreName, key)
 			maxCallsPerMinute = 8,
 			callCooldown = 8,
 			callsThisMinute = 0,
-			previousRefreshTime = currentTime,
-			previousCallTime = currentTime,
+			previousRefreshTick = currentTick,
+			previousCallTick = currentTick,
 		};
 		save = {
 			maxRetries = 3,
 			maxCallsPerMinute = 4,
 			callCooldown = 8,
 			callsThisMinute = 0,
-			previousRefreshTime = currentTime,
-			previousCallTime = currentTime,
+			previousRefreshTick = currentTick,
+			previousCallTick = currentTick,
 		}
 	}
 	
 	-- Setup information
-	local core = {}
-	self._core = core
-	core.dataStoreName = dataStoreName
-	core.dataStore = dataStoreName and key and dataStoreService:GetDataStore(dataStoreName)
-	core.key = key
-	core.player = key and players:GetPlayerByUserId(tonumber(tostring(key):match("%d+")))
-	core.errorMessageBase = "DataStore+ | Failed to %s DataKey '".. tostring(key).."' ("..dataStoreName.."): "
-	core.startData = {}
+	self.dataStoreName = dataStoreName
+	self.dataStore = dataStoreName and key and dataStoreService:GetDataStore(dataStoreName)
+	self.key = key
+	self.sessionId = httpService:GenerateGUID(false)
+	self.isNewUser = nil
+	self.isLoaded = false
+	self.loaded = maid:give(Signal.new())
+	self.player = nil
+	self.errorMessageBase = "DataStore+ | Failed to %s DataKey '".. tostring(key).."' ("..dataStoreName.."): "
+	self.startData = {}
 	
 	-- AutoSave
-	core.saveLoopInitialized = false
-	core.previousAutoSaveTime = currentTime
+	self._nextAutoSaveTick = currentTick
 	if self.autoSave then
 		self:initSaveLoop()
 	end
@@ -80,87 +87,92 @@ end
 
 
 
--- DATASTORE METHODS
+-- METHODS
 function User:loadAsync()
 	local callType = "load"
-	local core = self._core
-	local existingData = self.data
-	local rawData = self:protectedCall(callType, function(finalAttempt)
-		return core.dataStore:GetAsync(core.key)
+	self.isLoaded = false
+	
+	-- Retrieve previous perm data 
+	local permData = self:protectedCall(callType, function(finalAttempt)
+		return self.dataStore:GetAsync(self.key)
 	end)
-	--
-	if not rawData then
-		rawData = core.startData
-		self.isNewData = true
+	
+	-- Setup perm; if nothing found, apply start data
+	if not permData then
+		permData = self.startData
+		self.isNewUser = true
 	else
-		self.isNewData = false
+		self.isNewUser = false
 	end
-	--
-	local failData = existingData and existingData._failData
-	if failData then
-		failData:destroy()
+	for k,v in pairs(permData) do
+		self.perm[k] = v
 	end
-	local previousFailData = rawData._failData or {}
-	failData = TableModifiers.new(self)
-	for k,v in pairs(previousFailData) do
-		failData[k] = v
+	
+	-- Find and trigger any backup data
+	local backupData = permData._backupData
+	if backupData then
+		for name, content in pairs(backupData) do
+			if type(content) == "table" then
+				if #content > 0 then
+					for i,v in pairs(content) do
+						self.backup:insert(name, v)
+					end
+				else
+					for k,v in pairs(content) do
+						self.backup:pair(name, k, v)
+					end
+				end
+			else
+				self.backup:change(name, content, permData[name])
+			end
+		end
+		self.perm._backupData = nil
 	end
-	rawData._failData = failData
-	--
-	local data = existingData or TableModifiers.new(self)
-	for k,v in pairs(rawData) do
-		data[k] = v
-	end
-	--
-	self.data = data
-	self.dataLoaded:Fire()
-	--
-	if failData._tableUpdated then
-		self.failDataPresent:Fire(failData)
-	end
-	--
-	return data
+	
+	-- Complete
+	self.isLoaded = true
+	self.loaded:Fire()
+	return self.perm
 end
 
 function User:saveAsync()
 	local callType = "save"
-	local core = self._core
-	-- Check if anything needs to be saved
-	if not self.data or self.data._tableUpdated == false and self.data._failData._tableUpdated == false and self.onlySaveDataWhenChanged then
+	
+	-- Return if nothing needs saving
+	if self.perm._tableUpdated == false and self.backup._tableUpdated == false and self.onlySaveDataWhenChanged then
 		return false
 	end
+	
 	-- Save data
 	local backupAction = false
 	local success = self:protectedCall(callType, function(finalAttempt)
-		return core.dataStore:UpdateAsync(core.key, function(previousData)
-			local previousData = previousData or self.data
-			if previousData._dataId == self.data._dataId then
+		return self.dataStore:UpdateAsync(self.key, function(previousData)
+			local previousData = previousData or self.perm
+			if previousData._dataId == self.perm._dataId then
 				-- DataIds match, generate new unique DataId
-				self.data._dataId = httpService:GenerateGUID()
-				self.data._tableUpdated = false
+				self.perm._dataId = httpService:GenerateGUID()
+				self.perm._tableUpdated = false
 			elseif finalAttempt then
-				-- DataIds do not match, all retries failed, force add _failData to previousData and proceed to backup action 
-				warn(string.format(core.errorMessageBase.."DataIds do not match, all retries failed. Saved _failData and and proceeding to backup action.", callType))
-				previousData._failData = self.data._failData
+				-- DataIds do not match, all retries failed, force add backup data to previousData and proceed to backup action 
+				warn(string.format(self.errorMessageBase.."DataIds do not match, all retries failed. Saved backup data and and proceeding to backup action.", callType))
+				previousData._backupData = self.backup
 				backupAction = true
 				return previousData
 			else
 				-- DataIds do not match, abort save and retry
-				warn(string.format(core.errorMessageBase.."DataIds do not match, retrying save...", callType))
+				warn(string.format(self.errorMessageBase.."DataIds do not match, retrying save...", callType))
 				return nil
 			end
-			-- Clear _failData if required
-			local failData = self.data._failData
-			failData:clear()
 			-- Success, return data to be saved
-			return self.data
+			return self.perm
 		end)
 	end)
-	-- All retires failed, resort to backup action
+	
+	-- All retries failed, resort to backup action
 	if backupAction then
 		-- Teleport away (not recommended)
 		if self.teleportPlayerAwayOnFail then
-			local player = core.player
+			local player = self.player
 			if player then
 				teleportService:Teleport(game.PlaceId, player)
 				return "FinalAttempt: teleported away"
@@ -170,69 +182,69 @@ function User:saveAsync()
 		self:loadAsync()
 		return "FinalAttempt: reloaded data"
 	end
+	
 	-- Return
 	return success
 end
 
 function User:removeAsync()
-	local core = self._core
-	return core.dataStore:RemoveAsync(core.key)
+	return self.dataStore:RemoveAsync(self.key)
 end
 
 
 
 -- UTILITY METHODS
 function User:setStartData(startData)
-	local core = self._core
 	if type(startData) ~= "table" then
 		startData = {startData}
 	end
-	core.startData = startData
+	self.startData = startData
 end
 
-function User:initSaveLoop()
-	local core = self._core
+function User:initSaveLoop(autoSaveInterval)
 	local loopId = self.sessionId
-	if not core.saveLoopInitialized then
-		core.saveLoopInitialized = true
-		coroutine.wrap(function()
-			while self.autoSave and loopId == self.sessionId do
-				local currentTime = os.time()
-				if currentTime - core.previousAutoSaveTime >= self.autoSaveInterval then
-					core.previousAutoSaveTime = currentTime
-					self:saveAsync()
-				end
-				wait(1)
-			end
-			core.saveLoopInitialized = false
-		end)()
+	self.autoSaveInterval = tonumber(autoSaveInterval) or self.autoSaveInterval
+	if self.saveLoopInitialized then
+		return false
 	end
+	self.saveLoopInitialized = true
+	coroutine.wrap(function()
+		while self.autoSave and loopId == self.sessionId do
+			local currentTick = tick()
+			if currentTick >= self._nextAutoSaveTick then
+				self._nextAutoSaveTick = currentTick + self.autoSaveInterval
+				self:saveAsync()
+			end
+			RunService.Heartbeat:Wait()
+		end
+		self.saveLoopInitialized = nil
+	end)()
 end
 
 function User:protectedCall(callType, func)
-	local core = self._core
-	if not core.dataStore then
+	if not self.dataStore then
 		return {}
 	end
 	local callTypeInfo = self.callInfo[callType]
-	local errorMessageBaseStart = string.format(core.errorMessageBase, callType)
+	local errorMessageBaseStart = string.format(self.errorMessageBase, callType)
 	-- Call limit checks
+	local currentTick = tick()
 	if callTypeInfo.callsThisMinute > callTypeInfo.maxCallsPerMinute then
 		-- Has the max number of calls/minute been exeeded?
 		warn(string.format("%sExceeded maxCallsPerMinute (callsThisMinute = %s, maxCallsPerMinute = %s).", errorMessageBaseStart, callTypeInfo.callsThisMinute, callTypeInfo.maxCallsPerMinute))
 		return nil
-	elseif os.time() - callTypeInfo.previousCallTime < callTypeInfo.callCooldown then
+	elseif currentTick - callTypeInfo.previousCallTick < callTypeInfo.callCooldown then
 		-- Has the call been made before the cooldown? If so, delay until ready
 		repeat
 			wait(1)
-		until os.time() - callTypeInfo.previousCallTime >= callTypeInfo.callCooldown
+			currentTick = tick()
+		until currentTick - callTypeInfo.previousCallTick >= callTypeInfo.callCooldown
 	end
 	-- Update limit values
-	local currentTime = os.time()
 	callTypeInfo.callsThisMinute = callTypeInfo.callsThisMinute + 1
-	callTypeInfo.previousCallTime = currentTime
-	if currentTime - callTypeInfo.previousRefreshTime >= 60 then
-		callTypeInfo.previousRefreshTime = currentTime
+	callTypeInfo.previousCallTick = currentTick
+	if currentTick - callTypeInfo.previousRefreshTick >= 60 then
+		callTypeInfo.previousRefreshTick = currentTick
 		callTypeInfo.callsThisMinute = 0
 	end
 	-- Call function and retry if necessary
@@ -253,11 +265,7 @@ end
 
 function User:destroy()
 	self.sessionId = nil
-	for maidName, maid in pairs(self) do
-		if type(maid) == "table" and maid.clean then
-			maid:clean()
-		end
-	end
+	self._maid:clean()
 end
 
 
