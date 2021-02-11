219c5107226154b540588106f2883154718d42f4
1ForeverHD <ben.horton.finance@gmail.com>
2021-02-11 14:31:00 +0000

    Updated framework and completed loader extensions

--- src/Core/Server/Modules/Commands/init.lua
+++ src/Core/Server/Modules/Commands/init.lua
@@ -1,19 +1,41 @@
-local commands = {
+local main = require(game.Nanoblox)
+local Commands = {
 	array = {},
 	dictionary = {},
 }
---[[
-for _, module in pairs(script:GetChildren()) do
-	local moduleName = module.Name
-	local tagName = "_".. (moduleName:sub(1,1)):lower()..moduleName:sub(2)
-	local commandsToAdd = require(module)
-	for _, command in pairs(commandsToAdd) do
-		command.tags = (typeof(command.tags == "table") and command.tags) or {}
-		if not table.find(command.tags, tagName) then
-			table.insert(command.tags, tagName)
-		end
-		table.insert(commands.array, command)
-		commands.dictionary[command.name] = command
+
+
+
+local function setupCommands(group, tags)
+	local groupClass = group.ClassName
+	local thisTag = (groupClass == "Folder" or groupClass == "Configuration") and group.Name:lower()
+	local newTags = tags and {table.unpack(tags)} or {}
+	if thisTag then
+		table.insert(newTags, thisTag)
 	end
-end--]]
-return commands
+	for _, instance in pairs(group:GetChildren()) do
+		if instance:IsA("ModuleScript") then
+			local command = require(instance)
+			command.tags = (typeof(command.tags == "table") and command.tags) or {}
+			command.UID = instance.Name
+			print(instance.Name, "tags = ", table.unpack(newTags))
+			for _, tagToAdd in pairs(newTags) do
+				table.insert(command.tags, tagToAdd)
+			end
+			local client = instance:FindFirstChild("Client") or instance:FindFirstChild("client")
+			if client then
+				client.Name = instance.Name
+				client.Parent = main.shared.Modules.ClientCommands
+			end
+			table.insert(Commands.array, command)
+			Commands.dictionary[command.name] = command
+		else
+			setupCommands(instance, newTags)
+		end
+	end
+end
+setupCommands(script)
+
+
+
+return Commands
--- src/Core/init.lua
+++ src/Core/init.lua
@@ -10,6 +10,30 @@ function Core.init(loader)
     local shared = script.Shared
     local starterPlayer = client.StarterPlayer
     local Directory = require(shared.Assets.Directory)
+
+    -- This transfers extension items such as commands within the loader directly into the core
+    local extensionsToMove = {
+        Commands = server.Modules.Commands,
+        Core = true,
+    }
+    local extensions = loader and loader:FindFirstChild("Extensions")
+    if extensions then
+        for _, extensionGroup in pairs(extensions:GetChildren()) do
+            local destination = extensionsToMove[extensionGroup.Name]
+            if destination then
+                if extensionGroup.Name == "Core" then
+                    Directory.merge(extensionGroup, script, false, true)
+                else
+                    for _, item in pairs(extensionGroup:GetChildren()) do
+                        Directory.move(item, destination)
+                    end
+                    extensionGroup:Destroy()
+                end
+            end
+        end
+    end--]]
+
+    -- This splits up the core into its key areas
     Directory.createDirectory("ServerStorage.Nanoblox", {server})
     Directory.createDirectory("ReplicatedStorage.Nanoblox", {shared, client})
     Directory.createDirectory("StarterCharacterScripts", starterPlayer.StarterCharacterScripts:GetChildren())
