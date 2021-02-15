1826962331d143574c852452223c17039a541d55
1ForeverHD <ben.horton.finance@gmail.com>
2021-02-15 16:52:42 +0000

    Updated DataUtil

--- src/Core/Shared/Modules/DataUtil.lua
+++ src/Core/Shared/Modules/DataUtil.lua
@@ -54,29 +54,11 @@ function DataUtil.convertTimeStringToSeconds(timeString)
 		["o"] = 2628000, -- months
 		["y"] = 31540000, -- years
 	}
-	for value, unit in string.gmatch("(%d+)(%a)", timeString) do
-        totalSeconds += value * patternValues[unit]
-	end
-	return totalSeconds
-end
-
-local function convertTimeStringToSeconds(timeString)
-	local totalSeconds = 0
-	local patternValues = {
-		["s"] = 1, -- seconds
-		["m"] = 60, -- minutes
-		["h"] = 3600, -- hours
-		["d"] = 86400, -- days
-		["w"] = 604800, -- weeks
-		["o"] = 2628000, -- months
-		["y"] = 31540000, -- years
-	}
-	for value, unit in string.gmatch("(%d+)(%a)", timeString) do
+	for value, unit in string.gmatch(timeString, "(%d+)(%a)") do
 		totalSeconds += value * patternValues[unit]
 	end
 	return totalSeconds
 end
-print(convertTimeStringToSeconds("10d5h1s"))
 
 
 
